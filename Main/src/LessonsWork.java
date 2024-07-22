9ccbe747c66178ed808b29d4a9d310b33454b443
nadjafov05 <nadjafovp@gmail.com>
2024-07-22 12:08:26 +0400

    Sarkhan Rasullu Lesson Pratices

---
+++ Main/src/LessonsWork.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class LessonsWork {
+    public static void pratices1(){
+        System.out.println("Convert Int to String");
+        int number = 123;
+        String str = String.valueOf(number);
+
+        if(str.equals("123")){
+            System.out.println(number + " Translate the number into a string"+" : one hundred twenty-five");
+        }
+    }
+
+    public static void pratices2(){
+        System.out.println("Print all the numbers divisible by 2 from 0 to the given number");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("reqem daxil edin: ");
+        int enterNumber = sc.nextInt();
+
+        for (int i=0;i<=enterNumber;i++){
+            if (i%2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void pratices3(){
+        System.out.println("Swap two variables without using a third variable");
+        int a = 11;
+        int b = 22;
+
+        a = b + a; // 5
+        b = a - b; //3
+        a = a - b; //2
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+    public static void pratices4(){
+        System.out.println("Find the number of digits in the number.//12345");
+        int reqemler = 456789654;
+        String str = String.valueOf(reqemler);
+        System.out.println(str.length());
+    }
+}
---
+++ Main/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int reqemler = 456789654;
+        String str = String.valueOf(reqemler);
+        System.out.println(str.length());
+    }
+}
